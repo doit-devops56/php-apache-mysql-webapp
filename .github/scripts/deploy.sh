e9bf93bbceb9366f9680a74998ec90df975b95f9
KST <tgrouptgroup681.com>
2024-02-27 16:45:47 +0545

    shell file added

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/expect -f
+set timeout 120
+set host [lindex $argv 0]
+send_user "host: $host\n"
+spawn ssh -o StrictHostKeyChecking=no ubuntu@$host "git clone https://github.com/doit-devops56/php-apache-mysql-webapp.git; cd php-apache-mysql-webapp; docker compose -f compose.yml up -d"
+expect "ubuntu*\ password:"
+send -- "changeme\r"
+# sleep 5
+# send -- "ls /\r"
+# sleep 10
+# send -- "exit\r"
+expect eof
