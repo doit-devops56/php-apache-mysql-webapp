f92e44eae8b6b0241b04898e60d9b9c82811c506
tgrouptgroup681 <160731953+tgrouptgroup681@users.noreply.github.com>
2024-02-28 17:07:07 +0545

    Update user-data.sh

--- .github/scripts/user-data.sh
+++ .github/scripts/user-data.sh
@@ -21,8 +21,7 @@ sudo usermod -aG docker ubuntu
 sed -i 's/^PasswordAuthentication\s*no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
 
-<<<<<<< HEAD
+
 echo -e "changeme\nchangeme" | sudo -S passwd ubuntu
-=======
-echo -e "changeme\nchangeme" | sudo -S passwd ubuntu
->>>>>>> 8eb7e9c4ba84ddaa319da4c90186d786c54fd2f4
+
+
