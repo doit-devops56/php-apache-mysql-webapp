8b1e64657e7b713980aa6fcde5bbe14ed03e62f0
Sudarshan194 <sudarshanup1@gmail.com>
2024-02-29 17:49:43 +0545

    Update main.tf

--- .github/IaC/main.tf
+++ .github/IaC/main.tf
@@ -18,38 +18,38 @@ output "aws_vpc_id" {
   value = data.aws_vpc.default.id
 }
 
-#data "aws_security_groups" "test" {
-#
-#  filter {
-#    name   = "vpc-id"
-#    values = [data.aws_vpc.default.id]
-#  }
-#}
+data "aws_security_groups" "test" {
 
-resource "aws_security_group" "devops_sg" {
-  name        = "devops_sg"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "devops_sg"
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
+
+#resource "aws_security_group" "devops_sg" {
+#  name        = "devops_sg"
+#  vpc_id      = data.aws_vpc.default.id
+#
+#  ingress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "devops_sg"
+#  }
+#}
 
 
 data "template_file" "user_data" {
@@ -81,12 +81,12 @@ resource "aws_instance" "mern-instance" {
   instance_type = "t2.medium"
 
   subnet_id              = [for s in data.aws_subnet.default : s.id][0]
-  #vpc_security_group_ids = data.aws_security_groups.test.ids
-  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  vpc_security_group_ids = data.aws_security_groups.test.ids
+  #vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
 
   tags = {
-    Name    = "php-instance-feature8"
+    Name    = "mern-instance-main"
     Project = "devops"
   }
 
@@ -105,11 +105,11 @@ output "private_ip" {
 }
 
 
-output "aws_security_group" {
-  value = data.aws_security_groups.test.ids
-}
+#output "aws_security_group" {
+ 
+#value = data.aws_security_groups.test.ids
+#}
 
 output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.default : s.id]
 }
-
